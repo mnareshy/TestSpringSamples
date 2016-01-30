0b9640ce20fe7351047b22de49126892f53dc004
meghana <meghananaresh.m@gmail.com>
2016-01-30 15:52:52 +0530

    as

---
+++ src/com/test/di/TextMessage.java
@@ -0,0 +1,20 @@
+package com.test.di;
+
+public class TextMessage {
+	
+	public TextMessage(){}
+	
+	private String text;
+
+	public String getMsg() {
+		// TODO Auto-generated method stub
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+
+}
