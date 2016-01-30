052b0d82e11bdda56bc232d354ed84605550938a
meghana <meghananaresh.m@gmail.com>
2016-01-30 15:53:06 +0530

    s

---
+++ src/com/test/di/MainApp.java
@@ -0,0 +1,18 @@
+package com.test.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+	public static void main(String[] args) {
+//		ApplicationContext context = new ClassPathXmlApplicationContext("resources/beans-inheritance.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("resources/beans-di.xml");
+
+		HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+
+		objA.sayMessage();
+		
+
+		
+	}
+}
